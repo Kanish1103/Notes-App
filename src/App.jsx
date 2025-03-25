cf73d3ba3a1ed683e328f2262d925948091bfefa
Kanish <kanishkumar022@gmail.com>
2025-03-25 22:52:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 import NoteList from './components/NoteList';
 import NoteEditor from './components/NoteEditor';
 import { useState } from 'react';
@@ -16,19 +16,19 @@ function App() {
 
   return (
 
-      <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={<NoteList isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}
-            />
-            <Route path="/new" element={<NoteEditor isDarkMode={isDarkMode} toggleTheme={toggleTheme} />} />
-            <Route path="/edit/:index" element={<NoteEditor isDarkMode={isDarkMode} toggleTheme={toggleTheme} />} />
-          </Routes>
-        </Router>
-      </div>
-    
+    <div className={`app ${isDarkMode ? 'dark' : 'light'}`}>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<NoteList isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}
+          />
+          <Route path="/new" element={<NoteEditor isDarkMode={isDarkMode} toggleTheme={toggleTheme} />} />
+          <Route path="/edit/:index" element={<NoteEditor isDarkMode={isDarkMode} toggleTheme={toggleTheme} />} />
+        </Routes>
+      </Router>
+    </div>
+
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 )
